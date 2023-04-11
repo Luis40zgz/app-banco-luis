f99a0ae53bd94fbceb34164fa26aef6c5a7b3819
luis40 <luissg21@gmail.com>
2023-04-11 14:34:33 +0200

    Creando el generador de usuarios

--- fakeuser.js
+++ fakeuser.js
@@ -1,5 +1,15 @@
 const { faker } = require('@faker-js/faker')
 const { fs } = require('fs')
+const { writeFile } = require('fs/promises')
+
+async function writeToFile(fileName, data) {
+  try {
+    await writeFile(fileName, data)
+    console.log(`Wrote data to ${fileName}`)
+  } catch (error) {
+    console.error(`Got an error trying to write the file: ${error.message}`)
+  }
+}
 
 const formatDate = (fecha) => {
   const year = fecha.getFullYear()
@@ -11,3 +21,21 @@ const formatDate = (fecha) => {
     fecha.getDay().toString.length < 2 ? `0${fecha.getDay()}` : fecha.getDay()
   return `${year}-${month}-${day}`
 }
+
+const users = [
+  { name: 'Juan Sánchez', pin: 1111 },
+  { name: 'María Portazgo', pin: 2222 },
+  { name: 'Estefanía Pueyo', pin: 3333 },
+  { name: 'Javier Rodríguez', pin: 4444 },
+]
+
+const value = faker.finance.amount(-500, 1500)
+const date = formatDate(
+  faker.date.between('2020-01-01T00:00:00.000Z', '2023-01-01T00:00:00.000Z')
+)
+
+const generarDatos = () => {
+  const array = []
+  users.forEach((element) => {})
+}
+data = generarDatos()

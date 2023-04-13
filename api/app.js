85ba7135bedc99e5647b94ea0c0eb57b3c2671ab
Luis40zgz <luissg21@gmail.com>
2023-04-13 02:09:52 +0200

    Falta hacer funcionar la api

--- api/app.js
+++ api/app.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
-import indexRouter from './routes/index.js'
-import productsRouter from './routes/products.js'
+import './usersAccounts.json' assert { type: 'json' }
 import cors from 'cors'
 
 var router = express.Router()
@@ -17,10 +16,6 @@ const PORT = 3000
 const app = express()
 app.use(cors())
 app.use(morgan('dev'))
-
-// definir la ruta raíz
-app.use('/', indexRouter)
-app.use('/products', productsRouter)
 
 // arrancar el servidor por el puerto 3000
 console.log(`Api escuchando por el puert ${PORT}`)
